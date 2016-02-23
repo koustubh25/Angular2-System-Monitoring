c51e07c220bb47629ff0d81de75210c77eb43c7b
Koustubh <koustubh@eviry.com>
2016-02-23 19:01:08 +0900

    supervisor api noe accepts JSON body

--- src/Supervisor/SupervisorController.php
+++ src/Supervisor/SupervisorController.php
@@ -77,6 +77,12 @@ class SupervisorController implements ControllerProviderInterface
             return new JsonResponse($status);
         });
 
+        $app->before(function (Request $request) {
+            if (0 === strpos($request->headers->get('Content-Type'), 'application/json')) {
+                $data = json_decode($request->getContent(), true);
+                $request->request->replace(is_array($data) ? $data : array());
+            }
+        });
 
         return $controllers;
     }
