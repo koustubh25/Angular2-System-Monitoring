a7ff519f1f5eae8a580f9be1b9920350ecc0c084
Koustubh <koustubh@eviry.com>
2016-03-01 11:27:29 +0900

    Install script got pcp

--- external/install-pcp.sh
+++ external/install-pcp.sh
@@ -5,8 +5,6 @@ if [ "$(id -u)" != "0" ]; then
   exit 1
 fi
 
-
-#check for cent os
 #check for Distro
 distro=$(cat /etc/*release)
 if [[ ! "$distro" =~ "Amazon Linux AMI" ]]; then
@@ -15,17 +13,18 @@ if [[ ! "$distro" =~ "Amazon Linux AMI" ]]; then
     service pmwebd start
 else
     if !(type pcp 2>/dev/null); then
-    yum -y install git bison flex gcc-c++ perl-Tk-devel libmicrohttpd-devel
-    git clone git://git.pcp.io/pcp
-    yum-builddep -y pcp
-    cd pcp
-    ./configure --prefix=/usr --sysconfdir=/etc --localstatedir=/var --with-webapi
-    make
-    make install
-    ldconfig -v
+        yum -y install git bison flex gcc-c++ perl-Tk-devel libmicrohttpd-devel
+        git clone git://git.pcp.io/pcp
+        yum-builddep -y pcp
+        cd pcp
+        ./configure --prefix=/usr/local --sysconfdir=/etc --localstatedir=/var --with-webapi
+        make
+        groupadd -r pcp
+        useradd -c "Performance Co-Pilot" -g pcp -d /var/lib/pcp -M -r -s /usr/sbin/nologin pcp
+        make install
+        ldconfig -v
+    fi
     service pcp restart
     service pmwebd restart
-    /sbin/chkconfig pmlogger on
-    service pmlogger restart
     service pmlogger restart
 fi
