d66ad2e2dc2de774049fe0088b096e7e44cfd558
Koustubh <koustubh@eviry.com>
2016-03-02 11:50:40 +0900

    changed config

--- public/dashboard/app/config/config.ts
+++ public/dashboard/app/config/config.ts
@@ -5,19 +5,19 @@ export var config:any =
 {
 
 //Private server information
-BATCH_SERVERS_API_BASE : "http://batch-servers",
-BATCH_SERVERS_REFRESH_INTERVAL : 10,
+    BATCH_SERVERS_API_BASE : "http://bento.batch-servers.miovp.com",
+    BATCH_SERVERS_REFRESH_INTERVAL : 10,
 
 //Gearman
-"GEARMAN_API_BASE" : "http://bento/gearman",
-GEARMAN_REFRESH_INTERVAL : 3, //in seconds
+    "GEARMAN_API_BASE" : "http://bento.miovp.com/gearman",
+    GEARMAN_REFRESH_INTERVAL : 3, //in seconds
 
 //SUPERVISOR
-SUPERVISOR_API_BASE : "http://bento/supervisor",
-SUPERVISOR_REFRESH_INTERVAL : 3, //in seconds
+    SUPERVISOR_API_BASE : "http://bento.miovp.com/supervisor",
+    SUPERVISOR_REFRESH_INTERVAL : 3, //in seconds
 
-REQUEST_TIMEOUT : 120, //in seconds
-TOAST_MESSAGE_TIMEOUT: 5
+    REQUEST_TIMEOUT : 120, //in seconds
+    TOAST_MESSAGE_TIMEOUT: 5
 
 
 };
